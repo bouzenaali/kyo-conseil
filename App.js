9f835421438d5dd3b1dc7f1caba23d540ea2279e
cheima mezdour <jc_mezdour@esi.dz>
2023-03-11 10:27:51 +0100

    Settings completed

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import VoiceComponent from './Components/voice';
-import SettingsComponent from './Components/settings';
 import SettingsList from './Components/Settings1';
 export default function App() {
   return (
